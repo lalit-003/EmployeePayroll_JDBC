301a586d908f01ce212d36beee31b62d5aaa3a91
lalit-003 <gahlawatl7@gmail.com>
2020-11-03 02:15:59 +0530

    Added functionality to add new  employee data to json server

--- JDBC/src/test/java/com/bridgelabz/jdbc/JDBC/EmployeeRestAssured_Tests.java
+++ JDBC/src/test/java/com/bridgelabz/jdbc/JDBC/EmployeeRestAssured_Tests.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.jdbc.JDBC;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.Assert;
@@ -10,6 +12,7 @@ import com.google.gson.Gson;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class EmployeeRestAssured_Tests {
 
@@ -25,6 +28,15 @@ public class EmployeeRestAssured_Tests {
 		EmployeeData[] arrayOfEmps = new Gson().fromJson(response.asString(),EmployeeData[].class);
 		return arrayOfEmps;
 	}
+	
+	private Response addEmployeeToJsonServer(EmployeeData employeePayrollData) {
+          String empJson = new Gson().toJson(employeePayrollData);
+          RequestSpecification request = RestAssured.given();
+          request.header("Content-Type","application/json");
+          request.body(empJson);
+		return request.post("/employee_payroll");
+	}
+
 
 	//Test to retrieve entries from json server
 	@Test
@@ -36,4 +48,27 @@ public class EmployeeRestAssured_Tests {
 		long entries = employeePayroll.countEntries();
 		Assert.assertEquals(2,entries);
 	}
+	
+	//adding new employee to json server and than counting
+	@Test
+	public void givenNewEmployee_WhenAdded_ShouldMatchCountand201ResponseAndCount() throws SQLException
+	{
+		EmployeeData[] arrayOfEmps = getEmployeeList();
+		EmployeePayroll_DB employeePayroll;
+		employeePayroll = new EmployeePayroll_DB(Arrays.asList(arrayOfEmps));
+        
+		EmployeeData employeePayrollData = null;
+		employeePayrollData = new EmployeeData(0,"Mark Zuckerberg","M",300000.0,LocalDate.now());
+		Response response = addEmployeeToJsonServer(employeePayrollData);
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(201,statusCode);
+		
+		employeePayrollData = new Gson().fromJson(response.asString(),EmployeeData.class);
+		employeePayroll.addEmployeeToPayroll(employeePayrollData);
+		long entries = employeePayroll.countEntries();
+		Assert.assertEquals(3,entries);
+	}
+
+	
+		
 	}
